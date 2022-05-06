f4d8ad3f9a35958db0be175e5ebf7f8ecf9eb78c
sofiacrf <sofia.rojasflo@gmail.com>
2022-05-06 16:42:26 -0500

    added modal

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,36 +1,42 @@
+import { Modal } from './UI/Modal';
+
 class PlaceFinder {
-    constructor() {
-      const addressForm = document.querySelector('form');
-      const locateUserBtn = document.getElementById('locate-btn');
-  
-      locateUserBtn.addEventListener('click', this.locateUserHandler);
-      addressForm.addEventListener('submit', this.findAddressHandler);
-    }
-  
-    locateUserHandler() {
-      if (!navigator.geolocation) {
-        alert(
-          'Please enter an address.'
-        );
-        return;
-      }
-      navigator.geolocation.getCurrentPosition(
-        successResult => {
-          const coordinates = {
-            lat: successResult.coords.latitude + Math.random() * 50,
-            lng: successResult.coords.longitude + Math.random() * 50,
-          };
-          console.log(coordinates);
-        },
-        error => {
-          alert(
-            'Please enter an address manually!'
-          );
-        }
-      );
-    }
-  
-    findAddressHandler() {}
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
   }
-  
-  const placeFinder = new PlaceFinder();
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        'Please manually enter an address.'
+      );
+      return;
+    }
+    const modal = new Modal('loading-modal-content', 'Loading location, please wait!');
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      successResult => {
+        modal.hide();
+        const coordinates = {
+          lat: successResult.coords.latitude + Math.random() * 50,
+          lng: successResult.coords.longitude + Math.random() * 50,
+        };
+        console.log(coordinates);
+      },
+      error => {
+        modal.hide();
+        alert(
+          'Please manually enter an address'
+        );
+      }
+    );
+  }
+
+  findAddressHandler() {}
+}
+
+const placeFinder = new PlaceFinder();
